0bb22a731f16e66820f17d366e2b0e64a38fbb4a
Loraine Monteagudo García <loraine.monteagudo@gmail.com>
2019-12-10 11:36:53 -0700

    Finishing mining

--- blockchain.py
+++ blockchain.py
@@ -29,26 +29,40 @@ class Blockchain:
     def bestblock(self):
         return self.state[STATE_BEST]
     
+    def change_difficulty(self):
+        return self.state[STATE_BEST] % MINING_ROUND == 0 
+
+    @property
+    def last_state(self):
+        return self.state[STATE_CHAIN][self.state[STATE_BEST]]
+
     # This method performs the mining procedure.
     def mine(self, broadcast=None):
         # There is defined a class variable "mining_alive" that defines if the mining loop should be alive.
+        diff = 2**235
+        start_round = time.time()
         while self.mining_alive:
-            time.sleep(5)
             if self.pending:
-                data = {BLOCK_CONTENT: self.pending, BLOCK_MERKLE_ROOT: ''}
-                prev = self.state[STATE_CHAIN][self.state[STATE_BEST]]
-                data[BLOCK_HASH], data[BLOCK_NONCE], data[BLOCK_DIFFICULTY] = self.get_header(prev)
+                start = time.time()
+                data = {BLOCK_CONTENT: list(self.pending), BLOCK_MERKLE_ROOT: ''}
+                prev = self.last_state
+                data[BLOCK_HASH], data[BLOCK_NONCE], data[BLOCK_DIFFICULTY] = self.get_header(prev, diff)
                 data[BLOCK_INDEX] = prev[BLOCK_INDEX] + 1
                 data[BLOCK_PREV_HASH] = prev[BLOCK_HASH]
-                data[BLOCK_TIMESTAMP] = time.time()
+                data[BLOCK_TIMESTAMP] = time.time() - start
                 b = Block(data, self.container)
                 b.recalculate_tree()
                 self.setState(b)
                 print(b)
+                if self.change_difficulty():
+                    end_round = time.time()
+                    if end_round - start_round < MINING_TIME:
+                        diff >> 1
+                    elif end_round - start_round > MINING_TIME:
+                        diff <<  1
 
 
-    def get_header(self, prev_block):
-        initial_difficulty = prev_block[BLOCK_DIFFICULTY]
+    def get_header(self, prev_block, difficulty):
         hash_ = prev_block[BLOCK_HASH]
         
         # checks the current time
@@ -58,23 +72,20 @@ class Blockchain:
         new_block = 'transaction_' + hash_
 
         # find a valid nonce for the new block
-        hash_, nonce = self.proof_of_work(new_block)
+        hash_, nonce = self.proof_of_work(new_block, difficulty)
 
         end_time = time.time()
-        # TODO: Si el tiempo transcurrido excede un tiempo determinado, bajar la dificultad
-        return hash_, nonce, initial_difficulty
+        return hash_, nonce, difficulty
 
 
-    def proof_of_work(self, header):
+    def proof_of_work(self, header, difficulty):
         # calculate the difficulty target
-        diff = SPACE
-        init = start = time.time()
-        # target = 2**difficulty
-        for nonce in range(2**32):
+        diff = difficulty
+        for nonce in range(SPACE):
             text = f'{header}_{nonce}'
             h = hashlib.sha256(text.encode()).digest()
-            # check if this is a valid result, below the target
             n = int.from_bytes(h, byteorder=sys.byteorder)            
+            # check if this is a valid result, below the target
             if n < diff:
                 return h, nonce
         return None, nonce
@@ -119,12 +130,12 @@ if __name__ == "__main__":
     thr = Thread(target=miner, args=[broadcast])
     thr.start()
 
-    # while True:
-    #     message = input(">>> ")
-    #     if message == "exit":
-    #         B.mining_alive = False
-    #         break
-    #     B.add_content(str(BLOCK_CONTAINER.create(message)))
-    B.add_content(str(BLOCK_CONTAINER.create('Hola')))
-    B.add_content(str(BLOCK_CONTAINER.create('soy')))
-    B.add_content(str(BLOCK_CONTAINER.create('Hola')))
+    while True:
+        message = input(">>> ")
+        if message == "exit":
+            B.mining_alive = False
+            break
+        B.add_content(str(BLOCK_CONTAINER.create(message)))
+    # B.add_content(str(BLOCK_CONTAINER.create('Hola')))
+    # B.add_content(str(BLOCK_CONTAINER.create('soy')))
+    # B.add_content(str(BLOCK_CONTAINER.create('Hola')))
--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-MINING_TIME  = 200      # Tiempo q se supone q se mine un bloque
-MINING_ROUND = 100      # se mina mining_round bloques en mining_time segundos
+MINING_TIME  = 10      # Tiempo q se supone q se mine un bloque
+MINING_ROUND = 5        # se mina mining_round bloques en mining_time segundos
 MINING_LIMIT = 20       # cantidad máxima de mensajes q puede estar contenida en un bloque
 SPACE        = 2 ** 256 # espacio de llaves sobre el cual se aplica la dificultad
 
 from message import Message
 BLOCK_CONTAINER = Message
 
-GENESIS_BLOCK = "{\"index\": 0, \"nonce\": 0, \"timestamp\": 0, \"previous\": \"NONE\", \"difficulty\": 1000, \"content\": [\"{\\\"data\\\": \\\"Hello World From Blockchain\\\", \\\"hash\\\": \\\"1b4c878aea01ffd70803426843f28d0c8f83342175528685bd7cf4a671a8466e\\\"}\"], \"merkle\": \"5af7a60f7b6bc1a291096f1b85513a3a3faef6f4bd54175ab107978243f0388a\", \"hash\": \"cbc750e95e066eb3aa56f3392c3dbd34b8988e53ee8b899b1e3b6f48393cb638\"}"
+GENESIS_BLOCK = "{\"index\": 0, \"nonce\": 0, \"timestamp\": 0, \"previous\": \"NONE\", \"difficulty\": 1000, \"content\": [\"{\\\"data\\\": \\\"Hello World From Blockchain\\\", \\\"hash\\\": \\\"1b4c878aea01ffd70803426843f28d0c8f83342175528685bd7cf4a671a8466e\\\"}\"], \"merkle\": \"5af7a60f7b6bc1a291096f1b85513a3a3faef6f4bd54175ab107978243f0388a\", \"hash\": \"cbc750e95e066eb3aa56f3392c3dbd34b8988e53ee8b899b1e3b6f48393cb638\"}" 
